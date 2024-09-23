738b835c766981c37c35a988d620208ab9eeba4d
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-09-23 21:13:01 +0700

    update

--- user.js
+++ user.js
@@ -4,7 +4,7 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Fill SVG Color
 user_pref("svg.context-properties.content.enabled", true);
 
-// CSS's `:has()` selector 
+// CSS's `:has()` selector
 user_pref("layout.css.has-selector.enabled", true);
 
 // Integrated calculator at urlbar
@@ -22,6 +22,9 @@ user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
 
 // Who is bogus? (fixes Sidebery tab dragging on Linux)
 user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
+
+// Set dark theme for web appearance
+user_pref("layout.css.prefers-color-scheme.content-override", 2);
 
 // Enable DevTools on firefox
 user_pref("devtools.chrome.enabled", true);
