57bcc22edaf2ef7ae259e1e18b9424b9eb597119
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-11-02 11:57:48 +0000

    update

--- user.js
+++ user.js
@@ -20,7 +20,7 @@ user_pref("browser.urlbar.trimURLs", true);
 // GTK rounded corners
 user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
 
-// Who is bogus? (fixes Sidebery tab dragging on Linux)
+// Who is bogus? (fixes tab dragging on Linux)
 user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
 
 // Set dark theme for web appearance
